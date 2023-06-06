2513d87d7f54ab9d191f0c7a16e3854cd1c3ef1b
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-06-06 08:18:20 -0600

    Ultimo commit

--- src/components/Feedback.jsx
+++ src/components/Feedback.jsx
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import Button from './button'
 import styles from "../styles/feedback.module.css"
+import Statistics from './Statistics'
 
 class Feedback extends Component {
     // Initial State
@@ -12,35 +13,48 @@ class Feedback extends Component {
 
     // Changin the state when the user perfoms a +1
     counterGood = () => {
-        this.state({
-            good: this.state.good + 1
+        this.setState({
+            good: this.state.good + 1,
         })
     }
     counterNeutral = () => {
-        this.state({
-            neutral: this.state.neutral + 1
+        this.setState({
+            neutral: this.state.neutral + 1,
         })
     }
     counterBad = () => {
-        this.state({
-            bad: this.state.bad + 1
+        this.setState({
+            bad: this.state.bad + 1,
         })
     }
     
     render() {
         return (
-            <>
+            
                 <div>
                     <h1>Please Leave Feedback</h1>
 
                     <div className={styles.buttons__container}>
-                        <Button buttonTitle="God" inclickHandler={this.counterGood}/>
-                        <Button buttonTitle="Neutral" inclickHandler={this.counterNeutral}/>
-                        <Button buttonTitle="Bad" inclickHandler={this.counterBad}/>
+                        <Button buttonTitle="Good" onclickHandler={this.counterGood}/>
+                        <Button buttonTitle="Neutral" onclickHandler={this.counterNeutral}/>
+                        <Button buttonTitle="Bad" onclickHandler={this.counterBad}/>
                     </div>
-                    
+
+                    {
+                        this.state.good != 0 ||
+                        this.state.neutral != 0 ||
+                        this.state.bad != 0 ? (
+                            <Statistics
+                                good={this.state.good}
+                                neutral={this.state.neutral}
+                                bad={this.state.bad}
+                            />
+                        ) : (
+                            <h3> No Feedback given</h3>
+                        )
+                    }
                 </div>
-            </>
+            
         )
     }
 }
--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import styles from "../styles/statistics.module.css"
 
 class Statistics extends Component  {
     render() {
