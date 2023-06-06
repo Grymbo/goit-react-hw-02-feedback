9d4527366573c1de95be37842620a3096efe7a76
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2023-06-06 07:42:59 -0600

    Se agrega el componente Statistics.jsx

---
+++ src/components/Statistics.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react'
+
+class Statistics extends Component  {
+    render() {
+        const { good, neutral, bad } = this.props;
+
+        return (
+            <>
+                <h1>Statistics</h1>
+                <div className={styles.statistics__container}>
+                    <p>Good: { good }</p>
+                    <p>Neutral: { neutral }</p>
+                    <p>Bad: { bad }</p>
+                    <p>total : { good + bad + neutral }</p>
+                    <p>Positive Feedback: {((good / (good + bad + neutral)) * 100).toFixed(0)}%</p>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Statistics
